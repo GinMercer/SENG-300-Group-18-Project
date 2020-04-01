e21b410156ec78cf670cd660ac6f17a054ae3fd6
Sijia Yin <yinsijia1617@163.com>
2020-03-31 22:55:26 -0600

    add some function to editor and reviewer

--- src/loginwindow/Editor.java
+++ src/loginwindow/Editor.java
@@ -4,11 +4,16 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JToggleButton;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Editor extends JPanel {
 
@@ -16,9 +21,10 @@ public class Editor extends JPanel {
 	 * Create the panel.
 	 */
 	public Editor(JFrame frame, Authenticator auth) {
-		setBackground(Color.BLACK);
+		setBackground(Color.GRAY);
 		
 		JButton btnNewButton = new JButton("Back");
+		btnNewButton.setBounds(227, 52, 75, 29);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				login panel = new login(frame, auth);
@@ -26,12 +32,69 @@ public class Editor extends JPanel {
 				frame.revalidate();
 			}
 		});
+		setLayout(null);
 		add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("Editor Page");
-		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblNewLabel.setBounds(314, 43, 137, 39);
+		lblNewLabel.setForeground(Color.BLACK);
 		add(lblNewLabel);
+		
+		JComboBox comboBoxforpaperchooser = new JComboBox();
+		comboBoxforpaperchooser.setFont(new Font("Dialog", Font.PLAIN, 14));
+		comboBoxforpaperchooser.setModel(new DefaultComboBoxModel(new String[] {"haskell", "prolog", "java"}));
+		comboBoxforpaperchooser.setBounds(120, 170, 200, 39);
+		add(comboBoxforpaperchooser);
+		
+		JComboBox comboBoxfor1reveiwer = new JComboBox();
+		comboBoxfor1reveiwer.setFont(new Font("Dialog", Font.PLAIN, 14));
+		comboBoxfor1reveiwer.setModel(new DefaultComboBoxModel(new String[] {"Mike", "Tom", "Jerry"}));
+		comboBoxfor1reveiwer.setBounds(396, 170, 200, 39);
+		add(comboBoxfor1reveiwer);
+		
+		JComboBox comboBoxfor2reviewer = new JComboBox();
+		comboBoxfor2reviewer.setFont(new Font("Dialog", Font.PLAIN, 14));
+		comboBoxfor2reviewer.setModel(new DefaultComboBoxModel(new String[] {"Mike", "Tom", "Jerry"}));
+		comboBoxfor2reviewer.setBounds(396, 234, 200, 39);
+		add(comboBoxfor2reviewer);
+		
+		JComboBox comboBoxfor3reviewer = new JComboBox();
+		comboBoxfor3reviewer.setFont(new Font("Dialog", Font.PLAIN, 14));
+		comboBoxfor3reviewer.setModel(new DefaultComboBoxModel(new String[] {"Mike", "Tom", "Jerry"}));
+		comboBoxfor3reviewer.setBounds(396, 300, 200, 39);
+		add(comboBoxfor3reviewer);
+		
+		JLabel lblpaperchooser = new JLabel("Please choose a paper");
+		lblpaperchooser.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblpaperchooser.setBounds(120, 140, 200, 29);
+		add(lblpaperchooser);
+		
+		JLabel lblChoosereviewer = new JLabel("Please choose three reviewers");
+		lblChoosereviewer.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblChoosereviewer.setBounds(396, 140, 263, 29);
+		add(lblChoosereviewer);
+		
+		JLabel lblfirstreviewer = new JLabel("1");
+		lblfirstreviewer.setBounds(366, 180, 18, 16);
+		add(lblfirstreviewer);
+		
+		JLabel lblsecondreviewer = new JLabel("2");
+		lblsecondreviewer.setBounds(366, 244, 18, 16);
+		add(lblsecondreviewer);
+		
+		JLabel lblthirdreviewer = new JLabel("3");
+		lblthirdreviewer.setBounds(366, 310, 18, 16);
+		add(lblthirdreviewer);
+		
+		JButton btnfinishassign = new JButton("DONE");
+		btnfinishassign.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnfinishassign.setBounds(286, 378, 117, 29);
+		add(btnfinishassign);
 
 	}
-
 }
--- src/loginwindow/Reviewer.java
+++ src/loginwindow/Reviewer.java
@@ -4,11 +4,16 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JToggleButton;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Reviewer extends JPanel {
 
@@ -16,9 +21,10 @@ public class Reviewer extends JPanel {
 	 * Create the panel.
 	 */
 	public Reviewer(JFrame frame, Authenticator auth) {
-		setBackground(Color.DARK_GRAY);
+		setBackground(Color.GRAY);
 		
 		JButton btnNewButton = new JButton("Back");
+		btnNewButton.setBounds(227, 52, 75, 29);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				login panel = new login(frame, auth);
@@ -26,10 +32,33 @@ public class Reviewer extends JPanel {
 				frame.revalidate();
 			}
 		});
+		setLayout(null);
 		add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Reviewer Page");
-		add(lblNewLabel);
+		JLabel lblreveiwerpage = new JLabel("Reviewer Page");
+		lblreveiwerpage.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblreveiwerpage.setBounds(310, 42, 194, 40);
+		add(lblreveiwerpage);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Dialog", Font.PLAIN, 14));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"haskell", "prolog", "java"}));
+		comboBox.setBounds(170, 150, 220, 40);
+		add(comboBox);
+		
+		JLabel lblpapershouldview = new JLabel("Paper should view");
+		lblpapershouldview.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblpapershouldview.setBounds(180, 125, 220, 29);
+		add(lblpapershouldview);
+		
+		JButton btnDone = new JButton("DONE");
+		btnDone.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnDone.setBounds(421, 156, 117, 29);
+		add(btnDone);
 
 	}
 
